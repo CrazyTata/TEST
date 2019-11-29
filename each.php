3dec6f7389a8762ccfcfaf31d2341a010acfd5cf
CrazyTata <1027532238@qq.com>
2019-11-29 09:07:12 +0800

    aa

--- each.php
+++ each.php
@@ -7,4 +7,15 @@ $bar2 = each($foo);
 print_r($bar2);
 $bar3 = each($foo);
 print_r($bar3);
-?>
+
+$foo = array("bob", "fred", "jussi", "jouni", "egon", "marliese");
+$bar = each($foo);
+print_r($bar);
+/*Array
+(
+    [1] => bob
+    [value] => bob
+    [0] => 0
+    [key] => 0
+)*/
+//结论 each 返回数组中当前的键／值对并将数组指针向前移动一步
